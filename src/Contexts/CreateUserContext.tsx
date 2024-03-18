9f7e07df09b0e69cc136d5b49827eafeb13e16c6
Lucasss-laurentino <lucaselorenzohenrique@gmail.com>
2024-03-18 13:17:25 -0300

    verificação create user

--- src/Contexts/CreateUserContext.tsx
+++ src/Contexts/CreateUserContext.tsx
@@ -2,9 +2,14 @@ import { SetStateAction, createContext, useState } from "react";
 import { http } from "../http";
 
 type CreateUserType = {
-    create_user: (data: any) => void,
+    create_user: (data: any, tel: string) => void,
     gatilho_logado: boolean,
     setGatilho_logado: React.Dispatch<SetStateAction<boolean>>,
+
+    gatilho_loader: boolean,
+    setGatilho_loader: React.Dispatch<SetStateAction<boolean>>,
+    erroCreateUser: string,
+    erroUserExist: string,
 }
 
 export const CreateUserContext = createContext<CreateUserType>(null!);
@@ -13,21 +18,43 @@ export const CreateUserProvider = ({children}: {children: JSX.Element}) => {
 
     const [gatilho_logado, setGatilho_logado] = useState(false);
 
-    const create_user = (data: any) => {
+    const [gatilho_loader, setGatilho_loader] = useState(false);
 
-        http.post('/create_user', {data}).then((response) => {
+    const [erroCreateUser, setErroCreateUser] = useState('')
 
-            localStorage.setItem('token', response.data.token);
+    const [erroUserExist, setErroUserExist] = useState('')
 
-            setGatilho_logado(true)
-            
-        })
+    const create_user = (data: any, tel: string) => {
+
+        if(Number(tel)){
+
+            http.post('/create_user', {data, tel}).then((response) => {
+                
+                localStorage.setItem('token', response.data.token);
+    
+                setGatilho_logado(true)
+                
+            }).catch((response) => {
+
+                setErroUserExist(response.response.data.erro)
+                setGatilho_loader(false)
+                //setErroUserExist(response.data.erro)
+
+            })
+
+
+        } else {
+
+            setErroCreateUser('Número de telefone inválido')
+            setGatilho_loader(false);
+
+        }
     
     }
 
     return (
 
-        <CreateUserContext.Provider value={{create_user, gatilho_logado, setGatilho_logado}}>
+        <CreateUserContext.Provider value={{create_user, gatilho_logado, setGatilho_logado, gatilho_loader, setGatilho_loader, erroCreateUser, erroUserExist}}>
             {children}
         </CreateUserContext.Provider>
 
--- src/components/Page_create_user/index.tsx
+++ src/components/Page_create_user/index.tsx
@@ -18,7 +18,7 @@ const schema = yup
     email: yup.string().email('Email inválido').required('Campo obrigatório'),
     password: yup.string().min(6, 'Senha muito curta').required('Campo obrigatório'),
     repeat_password: yup.string().min(6, 'Senha muito curta').oneOf([yup.ref('password')], 'As senhas precisam ser iguais').required('Campo obrigatório'),
-    celular: yup.string().required('Campo obrigatório'),
+    celular: yup.string().min(13, 'Preencha o campo corretamente').max(13, 'Preencha o campo corretamente').required('Campo obrigatório'),
   })
   .required()
 
@@ -28,15 +28,22 @@ export const Page_create_user = () => {
 
     const { setCreateOrLogin, setTagColor, setUrl } = useContext(FormEditContext);
 
-    const { create_user, gatilho_logado } = useContext(CreateUserContext)
+    const { create_user, gatilho_logado, gatilho_loader, setGatilho_loader, erroCreateUser , erroUserExist} = useContext(CreateUserContext)
 
     const { setMenu } = useContext(MenuContext)
 
     const create_user_loader = (data: any) => {
 
+        const sem_parenteses_1 = data.celular.split('(')
+        const sem_parenteses_2 = sem_parenteses_1[1].split(')')
+        const tel_string = sem_parenteses_2.join('');
+        const tel = tel_string.split(' ').join('')
+
+
         setGatilho_loader(true)
 
-        create_user(data)
+        create_user(data, tel)
+        
         
     }
 
@@ -47,8 +54,6 @@ export const Page_create_user = () => {
         }
 
     }, [gatilho_logado])
-
-    const [gatilho_loader, setGatilho_loader] = useState(false);
 
     const {
         register,
@@ -96,7 +101,7 @@ export const Page_create_user = () => {
                 <div className="form-control-login">
                     <span className='span_login'>Número de celular</span>
                     <InputMask
-                        mask={"(99) 999999999"}
+                        mask={"(99)999999999"}
                         type="text"
                         className={errors.celular ? 'input_error' : 'input_login'}
                         placeholder={errors.celular ? '' : 'Digite seu email'}
@@ -104,6 +109,7 @@ export const Page_create_user = () => {
                     />
 
                     {errors.celular && <p className='text-error'>{errors.celular.message}</p>}
+                    {erroCreateUser != '' && <p className='text-error'>{erroCreateUser}</p>}
 
                 </div>
 
@@ -129,12 +135,15 @@ export const Page_create_user = () => {
                     {errors.repeat_password && <p className='text-error'>{errors.repeat_password.message}</p>}
                 </div>
 
+                {erroUserExist && <p className='text-error'>{erroUserExist}</p>}
+
                 <div className="div_text_form_login">
                     <p className='text_form_login'>
                         Ao clicar no botão abaixo "Criar conta", você concorda com nossos <strong className='color_strong'>termos de uso</strong>
                         e a nossa <strong className='color_strong'>Politica de privacidade</strong> e confirma ter mais de 18 anos.
                     </p>
                 </div>
+
                 {!gatilho_loader ?
                 <button type='submit' className='btn_criar_conta'>Criar conta</button>
                 :
